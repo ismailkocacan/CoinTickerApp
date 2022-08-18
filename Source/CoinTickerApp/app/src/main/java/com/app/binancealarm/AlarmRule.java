e3a2be076d1736403555ccd99ef006069373bc66
ismailkocacan <ismailkocacan@gmail.com>
2022-08-18 14:29:59 +0300

    refactor

--- Source/CoinTickerApp/app/src/main/java/com/app/binancealarm/AlarmRule.java
+++ Source/CoinTickerApp/app/src/main/java/com/app/binancealarm/AlarmRule.java
@@ -95,3 +95,90 @@ public class AlarmRule {
     }
 }
 
+
+class AlarmRuleAsk extends AlarmRule{
+
+    public AlarmRuleAsk(){
+
+    }
+
+    public AlarmRuleAsk(Setting setting){
+        try{
+            setValues(setting);
+            if (setting.getValue3().trim().length() > 0)
+                setValueAsDouble(Double.parseDouble(setting.getValue3().trim()));
+        }catch (Exception e){
+            App.log(e);
+        }
+    }
+}
+
+class AlarmRuleBuy extends AlarmRule{
+
+    public AlarmRuleBuy(){
+
+    }
+
+    public AlarmRuleBuy(Setting setting){
+        try{
+            setValues(setting);
+            if (setting.getValue3().trim().length() > 0)
+                setValueAsDouble(Double.parseDouble(setting.getValue3().trim()));
+        }catch (Exception e){
+            App.log(e);
+        }
+    }
+}
+
+class AlarmRuleHighLow extends AlarmRule{
+
+    public AlarmRuleHighLow(){
+
+    }
+    public AlarmRuleHighLow(Setting setting){
+        try{
+            setValues(setting);
+            if (setting.getValue3().trim().length() > 0)
+                setValueAsBool(Boolean.parseBoolean(setting.getValue3().trim().trim()));
+        }catch (Exception e){
+            App.log(e);
+        }
+    }
+}
+
+
+/** Son fiyat için alarm kuralı verisini tutan sınıftır.
+ *  Sadece son fiyat'da priceChange değeri olduğu için bu şekilde
+ *  alt sınıf olarak yazılmıştır.
+ * */
+class  AlarmRuleLastOrder extends  AlarmRule{
+    private Double priceChange;
+
+    public AlarmRuleLastOrder(){
+
+    }
+
+    public AlarmRuleLastOrder(Setting setting){
+        try{
+            setValues(setting);
+            if (setting.getValue3() != null){
+                if (setting.getValue3().length() > 0)
+                    setValueAsDouble(Double.parseDouble(setting.getValue3()));
+            }
+            if (setting.getValue4() != null){
+                if (setting.getValue4().length() > 0)
+                    setPriceChange(Double.parseDouble(setting.getValue4()));
+            }
+        }catch (Exception e){
+            App.log(e);
+        }
+    }
+
+    public Double getPriceChange() {
+        return priceChange;
+    }
+
+    public void setPriceChange(Double priceChange) {
+        this.priceChange = priceChange;
+    }
+}
--- Source/CoinTickerApp/app/src/main/java/com/app/binancealarm/AlarmRuleStrategy.java
+++
@@ -1,5 +0,0 @@
-package com.app.binancealarm;
-
-public interface AlarmRuleStrategy {
-    boolean execute(AlarmRule alarmRule, TickerBinance ticker);
-}
